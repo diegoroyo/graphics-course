6729b47eb7381c834c5358d648b19c6628bc3528
diegoroyo <diego.roymen@gmail.com>
2019-10-03 16:03:46 +0200

    Cálculo de direcciones entrada/salida y colisión con planeta

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -28,8 +28,18 @@ int main() {
 
     // tenemos bases ijk de ambas estaciones
     // calcular v (posicion estacion 2 - estacion 1)
+    Vec4 v = station2.positionUCS - station1.positionUCS;
     // pasar v a base de estacion 1 (expresar UCS en base de est. 1)
+    Mat4 cob1 = Mat4.changeOfBasis(station1.i, station1.j, station1.k, station1.positionUCS);
+    Vec4 outDir = cob1 * v;
     // lo mismo para estacion 2
+    Mat4 cob2 = Mat4.changeOfBasis(station2.i, station2.j, station2.k, station2.positionUCS);
+    Vec4 incDir = cob2 * (v * -1.0f);
+
+    std::cout << "Outgoing direction: " << outDir << std::endl;
+    std::cout << "Incoming direction: " << incDir << std::endl;
     
     // se choca con el planeta si prod. escalar k * dir(salida|entrada) es < 0
+    if (station1.k * outDir < 0) std::cout << "Collision in outgoing direction" << std::endl;
+    if (station2.k * incDir < 0) std::cout << "Collision in incoming direction" << std::endl;
 }
