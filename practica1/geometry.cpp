825dd00e78966f832166c4d53faca1522fe2d518
diegoroyo <diego.roymen@gmail.com>
2019-10-07 12:42:16 +0200

    Formateo de código de geometry y añadidas algunas funciones
    - Formateo de código (espacios, etc)
    - Añadidos comentarios
    - Añadida Mat4::identity() para la matriz identidad
    - Modificadas las funciones static para que usen identity
    - Modificado Vec4::normalize para que acepte un módulo para
      el vector (ej: v.normalize(2) -> v tiene módulo 2)
    - Modificado Mat4::Mat4 para que acepte un número
      (ej: Mat4(2.0f) llena una matriz con todo 2.0f)
    - Ahora se pueden mostrar matrices por pantalla (cout << m)

--- practica1/geometry.cpp
+++ practica1/geometry.cpp
@@ -1,81 +1,158 @@
 #include "geometry.h"
 
-// dot product
-float dot(const Vec4& u, const Vec4 &v)  {
-    return u.x * v.x + u.y * v.y + u.z * v.z;
-}
-// cross product
-Vec4 cross(const Vec4& u, const Vec4 &v)  {
-    return Vec4(u.y * v.z - u.z * v.y,   // uyvz-uzvy
-                u.z * v.x - u.x * v.z,   // uzvx-uxvz
-                u.x * v.y - u.y * v.x,   // uxvy-uyvx
-                0.0f); // es dirección
-}
+/// Vectors ///
 
 std::ostream &operator<<(std::ostream &s, Vec4 &vector) {
-    s << "(" << vector.x << ", " << vector.y << ", " << vector.z << ", " << vector.w << ")";
+    s << "(" << vector.x << ", " << vector.y << ", "
+             << vector.z << ", " << vector.w << ")";
     return s;
 }
 
-Mat4::Mat4() {
+// dot product (ignores 4th component)
+float dot(const Vec4 &u, const Vec4 &v) {
+    return u.x * v.x + u.y * v.y + u.z * v.z;
+}
+// cross product (ignores 4th component)
+Vec4 cross(const Vec4 &u, const Vec4 &v) {
+    return Vec4(u.y * v.z - u.z * v.y,  // uyvz-uzvy
+                u.z * v.x - u.x * v.z,  // uzvx-uxvz
+                u.x * v.y - u.y * v.x,  // uxvy-uyvx
+                0.0f);                  // make it a direction vector
+}
+
+/// Matrices ///
+
+// fill constructor
+Mat4::Mat4(float elem) {
     for (int i = 0; i < 16; ++i) {
-        raw[i]=0.0;
+        raw[i] = elem;
     }
 }
 
-Mat4::Mat4(Vec4 u, Vec4 v, Vec4 w, Vec4 o) {
+// column vectors constructor
+Mat4::Mat4(Vec4 &u, Vec4 &v, Vec4 &w, Vec4 &o) {
     for (int i = 0; i < 4; ++i) {
-       m[i][0]= u.raw[i];
+        m[i][0] = u.raw[i];
     }
     for (int i = 0; i < 4; ++i) {
-        m[i][1]= v.raw[i];
+        m[i][1] = v.raw[i];
     }
     for (int i = 0; i < 4; ++i) {
-        m[i][2]= w.raw[i];
+        m[i][2] = w.raw[i];
     }
     for (int i = 0; i < 4; ++i) {
-        m[i][3]= o.raw[i];
+        m[i][3] = o.raw[i];
     }
 }
 
-/* 1        0           0       0
-* 0    cos _incl  -sin _incl   0
-* 0    sin _incl  cos _incl    0
-* 0        0           0       1*/
-Mat4 Mat4::rotationX(float _incl){
-    return Mat4 (Vec4 (1,0,0,0), Vec4 (0, std::cos(_incl), std::sin(_incl),0), Vec4 (0, - std::sin(_incl), std::cos(_incl),0),
-            Vec4 (0,0,0,1));
-}
-Mat4 Mat4::rotationY(float _incl){
-    return Mat4 (Vec4 (std::cos(_incl),0,-std::sin(_incl),0), Vec4 (0, 1, 0,0), Vec4 (std::sin(_incl), 0, std::cos(_incl),0),
-                 Vec4 (0,0,0,1));
-}
-Mat4 Mat4::rotationZ(float _incl){
-    return Mat4 (Vec4 (std::cos(_incl),std::sin(_incl),0,0), Vec4 (-std::sin(_incl), std::cos(_incl), 0,0), Vec4 (0, 0, 1,0),
-                 Vec4 (0,0,0,1));
+/**
+ *  1 0 0 0
+ *  0 1 0 0
+ *  0 0 1 0
+ *  0 0 0 1
+ */
+Mat4 Mat4::identity() {
+    Mat4 iden;
+    for (int i = 0; i < 4; ++i) {
+        iden[i][i] = 1.0f;
+    }
+    return iden;
 }
 
+/**
+ *  1 0 0 x
+ *  0 1 0 y
+ *  0 0 1 z
+ *  0 0 0 1
+ */
 Mat4 Mat4::translation(float xT, float yT, float zT) {
-    Mat4 res;
-    res.raw[0]=1;
-    res.raw[3]=xT;
-    res.raw[5]=1;
-    res.raw[7]=yT;
-    res.raw[10]=1;
-    res.raw[11]=zT;
-    res.raw[15]=1;
+    Mat4 res = Mat4::identity();
+    res.raw[3] = xT;
+    res.raw[7] = yT;
+    res.raw[11] = zT;
+    return res;
 }
 
-Mat4 Mat4::scale(float xS, float yS, float zS){
-    Mat4 res;
-    res.raw[0]=xS;
-    res.raw[5]=yS;
-    res.raw[10]=zS;
-    res.raw[15]=1;
+/* 
+ * 1   0     0   0
+ * 0  cos  -sin  0
+ * 0  sin   cos  0
+ * 0   0     0   1
+ */
+Mat4 Mat4::rotationX(float rad) {
+    Mat4 res = Mat4::identity();
+    res.raw[5] = std::cos(rad);
+    res.raw[6] = -std::sin(rad);
+    res.raw[9] = std::sin(rad);
+    res.raw[10] = std::cos(rad);
+    return res;
 }
 
-
-Mat4 Mat4::changeOfBasis(Vec4 u, Vec4 v, Vec4 w, Vec4 o){
-    return Mat4 (u,v,w,o);
+/* 
+ *  cos  0  sin  0
+ *   0   1   0   0
+ * -sin  0  cos  0
+ *   0   0   0   1
+ */
+Mat4 Mat4::rotationY(float rad) {
+    Mat4 res = Mat4::identity();
+    res.raw[0] = std::cos(rad);
+    res.raw[2] = std::sin(rad);
+    res.raw[8] = -std::sin(rad);
+    res.raw[11] = std::cos(rad);
+    return res;
 }
 
+/* 
+ * cos  -sin  0  0
+ * sin   cos  0  0
+ *  0     0   1  0
+ *  0     0   0  1
+ */
+Mat4 Mat4::rotationZ(float rad) {
+    Mat4 res = Mat4::identity();
+    res.raw[0] = std::cos(rad);
+    res.raw[1] = -std::sin(rad);
+    res.raw[4] = std::sin(rad);
+    res.raw[5] = std::cos(rad);
+    return res;
+}
+
+/**
+ * x 0 0 0
+ * 0 y 0 0
+ * 0 0 z 0
+ * 0 0 0 1
+ */
+Mat4 Mat4::scale(float xS, float yS, float zS) {
+    Mat4 res = Mat4::identity();
+    res.raw[0] = xS;
+    res.raw[5] = yS;
+    res.raw[10] = zS;
+    return res;
+}
+
+/**
+ * ux vx wx ox
+ * uy vy wy oy
+ * uz vz wz oz
+ * uw vw ww ow
+ * 
+ * If directions/points are correct, then
+ * uw = vw = ww = 0, ow = 1
+ */
+Mat4 Mat4::changeOfBasis(Vec4 &u, Vec4 &v, Vec4 &w, Vec4 &o) {
+    return Mat4(u, v, w, o);
+}
+
+std::ostream &operator<<(std::ostream &s, Mat4 &matrix) {
+    s << std::endl;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            s << matrix[i][j] << ' ';
+        }
+        s << std::endl;
+    }
+    s << std::endl;
+    return s;
+}
--- practica1/geometry.h
+++ practica1/geometry.h
@@ -1,9 +1,11 @@
 #pragma once
+
 #include <cmath>
 #include <ostream>
+#include <iomanip>
 #include <vector>
 
-
+/// Vectors ///
 
 struct Vec4 {
     union {
@@ -16,6 +18,10 @@ struct Vec4 {
     Vec4() : x(0), y(0), z(0), w(0) {}
     Vec4(float _w) : x(0), y(0), z(0), w(_w) {}
     Vec4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
+
+    inline float operator[](const int i) const {
+        return raw[i];
+    }
     inline Vec4 operator+(const Vec4 &other) const {
         return Vec4(x + other.x, y + other.y, z + other.z, w + other.w);
     }
@@ -25,72 +31,75 @@ struct Vec4 {
     inline Vec4 operator*(const float f) const {
         return Vec4(x * f, y * f, z * f, w * f);
     }
-    float module() const { return std::sqrt(x * x + y * y + z * z); }
-    Vec4 normalize() {
-        return Vec4(*this) * (1 / module());
+    float module() const {
+        return std::sqrt(x * x + y * y + z * z);
     }
-    inline float operator[](const int i) const{
-        return raw[i];
+    Vec4 normalize(float l = 1) {
+        return Vec4(*this) * (l / module());
     }
+
     friend std::ostream &operator<<(std::ostream &s, Vec4 &vector);
 };
 
-float dot(const Vec4& u, const Vec4 &v);
-Vec4 cross(const Vec4& u, const Vec4 &v);
+float dot(const Vec4 &u, const Vec4 &v);
+Vec4 cross(const Vec4 &u, const Vec4 &v);
 
-struct Mat4{
-   union {
-       struct {
-           float m[4][4];
-       };
-       float raw[16];
-   };
+/// Matrices ///
 
-    Mat4();
+struct Mat4 {
+    union {
+        struct {
+            float m[4][4];
+        };
+        float raw[16];
+    };
 
-    Mat4(Vec4 u, Vec4 v, Vec4 w, Vec4 o);
-    inline float* operator[](const int i) {
+    Mat4(float elem = 0.0f); // set all elements to "elem"
+    Mat4(Vec4 &u, Vec4 &v, Vec4 &w, Vec4 &o); // columns constructor
+
+    // Transformation matrices
+    static Mat4 identity();
+    static Mat4 translation(float xT, float yT, float zT);
+    static Mat4 scale(float xS, float yS, float zS);
+    static Mat4 rotationX(float rad);
+    static Mat4 rotationY(float rad);
+    static Mat4 rotationZ(float rad);
+    static Mat4 changeOfBasis(Vec4 &u, Vec4 &v, Vec4 &w, Vec4 &o);
+
+    inline float *operator[](const int i) {
         return m[i];
     }
-    //Multiplicacion de matrices
 
-    inline Mat4 operator*(const Mat4& a) const {
+    // matrix * matrix
+    inline Mat4 operator*(const Mat4 &a) const {
         Mat4 res;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 for (int k = 0; k < 4; k++) {
-                    res[i][j] += m[i][k] * a.m[k][j];
+                    res.m[i][j] += m[i][k] * a.m[k][j];
                 }
             }
         }
         return res;
     }
-
-    inline Vec4 operator* (const Vec4& v) const {
+    // matrix * vector
+    inline Vec4 operator*(const Vec4 &v) const {
         Vec4 res;
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
-                res.raw[i] += m[i][j]*v[j];
+                res.raw[i] += m[i][j] * v[j];
             }
         }
         return res;
     }
-    // matriz rotacion (eje, angulo)
-    static Mat4 rotationX(float _incl);
-    static Mat4 rotationY(float _incl);
-    static Mat4 rotationZ(float _incl);
+    // TODO
+    Mat4 inverse() const {
+        return *this;
+    }
+    // TODO
+    Mat4 transpose() const {
+        return *this;
+    }
 
-    //matriz traslacion (x, y, z)
-    static Mat4 translation(float xT, float yT, float zT);
-
-    //matriz escalado (x,y,z)
-    static Mat4 scale(float xS, float yS, float zS);
-    //TODO: matriz inversa
-
-
-    //matriz cambio de base
-    static Mat4 changeOfBasis(Vec4 u, Vec4 v, Vec4 w, Vec4 o);
-
-
-
+    friend std::ostream &operator<<(std::ostream &s, Mat4 &matrix);
 };
