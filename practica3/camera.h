47b77f52f85c33dbc2624ed2b66c14500ace3790
Yasmina97 <anaximandroabernanthy@gmail.com>
2019-11-24 21:57:58 +0100

    Arreglado ratio imagen. Constructor de camara desde tamaño de imagen (sin acabar)

--- practica3/camera.h
+++ practica3/camera.h
@@ -15,6 +15,9 @@ class Camera {
     Camera(Vec4 _origin, Vec4 _forward, Vec4 _up, Vec4 _right)
         : origin(_origin), forward(_forward), up(_up), right(_right) {}
 
+    Camera(Vec4 _origin, Vec4 _forward, Vec4 _up, int width, int height);
+
+
     // Generate an image render (see implementation)
     PPMImage render(int width, int height, int rpp,
                     const FigurePtr &sceneRootNode,
