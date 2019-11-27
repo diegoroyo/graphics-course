19d6cd70167ba808e505af63c32023a814369b83
diegoroyo <diego.roymen@gmail.com>
2019-11-27 11:27:42 +0100

    Constructor por aspectRatio

--- practica3/camera.cpp
+++ practica3/camera.cpp
@@ -1,14 +1,5 @@
 #include "camera.h"
 
-Camera::Camera(const Vec4 _origin, const Vec4 _forward, const Vec4 _up,
-               int width, int height)
-    : origin(_origin), forward(_forward), up(_up) {
-    forward = forward.normalize();
-    up = up.normalize();
-    right = cross(up, forward);
-    // TODO: calcular base de la camara en coordenadas del mundo
-}
-
 Vec4 Camera::cameraToWorld(const Vec4 &v) {
     // static to it doesn't construct the matrix more than once
     static Mat4 cob(right, up, forward, origin);
@@ -23,20 +14,12 @@ PPMImage Camera::render(int width, int height, int rpp,
     PPMImage result(width, height);
     result.fillPixels(backgroundColor);
 
-    double ratio = (double)width / (double)height;
-
-    up.normalize();
-    right.normalize();
-    right = right * ratio;
-
     // Iterate through [-1, 1] * [-1, 1] (camera's local space)
-    // First point is at ((-1 + 1/w)*ratio, 1 - 1/h, 1) in local space
+    // First point is at (-1 + 1/w, 1 - 1/h, 1) in local space
     Vec4 direction = cameraToWorld(
-        Vec4((-1.0f + 1.0f / width) * ratio, 1.0f - 1.0f / height, 1.0f, 0.0f));
-
+        Vec4(-1.0f + 1.0f / width, 1.0f - 1.0f / height, 1.0f, 0.0f));
     // Distance to next horizontal/vertical pixel
-    const Vec4 deltaX =
-        cameraToWorld(Vec4((2.0f / width) * ratio, 0, 0.0f, 0.0f));
+    const Vec4 deltaX = cameraToWorld(Vec4(2.0f / width, 0, 0.0f, 0.0f));
     const Vec4 deltaY = cameraToWorld(Vec4(0, -2.0f / height, 0.0f, 0.0f));
     for (int y = 0; y < height; ++y) {
         Vec4 originalDirection = direction;
--- practica3/camera.h
+++ practica3/camera.h
@@ -15,7 +15,11 @@ class Camera {
     Camera(Vec4 _origin, Vec4 _forward, Vec4 _up, Vec4 _right)
         : origin(_origin), forward(_forward), up(_up), right(_right) {}
 
-    Camera(Vec4 _origin, Vec4 _forward, Vec4 _up, int width, int height);
+    Camera(Vec4 _origin, Vec4 _forward, Vec4 _up, float _aspectRatio)
+        : origin(_origin),
+          forward(_forward),
+          up(_up),
+          right(cross(forward, up).normalize() * up.module() * _aspectRatio) {}
 
     // Generate an image render (see implementation)
     PPMImage render(int width, int height, int rpp,
