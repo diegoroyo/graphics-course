06c8c47c6575fa24c27c2bba7688220189e1200a
diegoroyo <diego.roymen@gmail.com>
2020-02-02 00:07:04 +0100

    Corrections on calculations of direct light
    I may or may not have been calculating direct light wrong this whole time :-)

--- lib/scene/scene.cpp
+++ lib/scene/scene.cpp
@@ -6,46 +6,23 @@ bool Scene::intersection(const Ray& ray, RayHit& hit) const {
 }
 
 RGBColor Scene::directLight(const RayHit& hit, const Vec4& wo) const {
-    RGBColor result = RGBColor::Black;
+    RGBColor result(0.0f, 0.0f, 0.0f);
     // Check all lights in the scene
     for (auto& light : lights) {
         Vec4 wi = light.point - hit.point;
         float norm = wi.module();
+        wi = wi * (1.0f / norm);
         RayHit hit;
-        Ray ray(light.point, wi * (-1.0f / norm), this->air);
+        Ray ray(light.point, wi * -1.0f, this->air);
         // Check if theres direct view from light to point
         if (this->intersection(ray, hit) &&
             std::abs(hit.distance - norm) < 1e-5f &&
             dot(hit.normal, ray.direction) < 1e-5f) {
             // Add light's emission to the result
-            RGBColor inEmission =
-                light.emission * (1.0f / (norm * norm)) * dot(hit.normal, wi);
+            RGBColor inEmission = light.emission * (1.0f / (norm * norm)) *
+                                  dot(hit.normal, wi);
             result = result + hit.material->evaluate(inEmission, hit, wi, wo);
         }
     }
     return result;
-}
-
-RGBColor Scene::directPhoton(const RayHit& hit, const Vec4& wo,
-                             const float epp) const {
-    RGBColor result = RGBColor::Black;
-    // Check all lights in the scene
-    for (auto& light : lights) {
-        Vec4 wi = light.point - hit.point;
-        float norm = wi.module();
-        RayHit hit;
-        Ray ray(light.point, wi * (-1.0f / norm), this->air);
-        // Check if theres direct view from light to point
-        if (this->intersection(ray, hit) &&
-            std::abs(hit.distance - norm) < 1e-5f &&
-            dot(hit.normal, ray.direction) < 1e-5f) {
-            // Add light's emission to the result
-            RGBColor inEmission = light.emission;
-            // Normalize direct light to photon light
-            inEmission = inEmission * (epp / light.emission.max());
-            result = result + hit.material->evaluate(inEmission, hit, wi, wo) *
-                                  dot(hit.normal, wi);
-        }
-    }
-    return result * (1.0f / (4.0f * M_PI));  // normalize solid angle on sphere
 }
